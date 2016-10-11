baa2b75a2e4a76f18612a57f7d3ae964cb414e43
Vincent Versnel <vincent@versnelhifi.nl>
2016-10-11 13:38:23 +0200

    Collision w/ Controllers

--- Assets/Scripts/WandController.cs
+++ Assets/Scripts/WandController.cs
@@ -13,7 +13,7 @@ public class WandController : MonoBehaviour
     private bool triggerButtonUp = false;
     public bool triggerButtonPressed = false;
 
-    private Valve.VR.EVRButtonId dPad_Up = Valve.VR.EVRButtonId.k_EButton_DPad_Up;
+    /*private Valve.VR.EVRButtonId dPad_Up = Valve.VR.EVRButtonId.k_EButton_DPad_Up;
     private bool dPad_UpDown = false;
     private bool dPad_UpUp = false;
     public bool dPad_UpPressed = false;
@@ -31,7 +31,7 @@ public class WandController : MonoBehaviour
     private Valve.VR.EVRButtonId dPad_Left = Valve.VR.EVRButtonId.k_EButton_DPad_Left;
     private bool dPad_LeftDown = false;
     private bool dPad_LeftUp = false;
-    public bool dPad_LeftPressed = false;
+    public bool dPad_LeftPressed = false;*/
 
     private SteamVR_Controller.Device controller {  get { return SteamVR_Controller.Input((int)trackedObj.index); } }
     private SteamVR_TrackedObject trackedObj;
@@ -58,7 +58,7 @@ public class WandController : MonoBehaviour
         triggerButtonUp = controller.GetPressUp(triggerButton);
         triggerButtonPressed = controller.GetPress(triggerButton);
 
-        dPad_UpDown = controller.GetPressDown(dPad_Up);
+        /*dPad_UpDown = controller.GetPressDown(dPad_Up);
         dPad_UpUp = controller.GetPressUp(dPad_Up);
         dPad_UpPressed = controller.GetPress(dPad_Up);
 
@@ -72,7 +72,7 @@ public class WandController : MonoBehaviour
 
         dPad_LeftDown = controller.GetPressDown(dPad_Left);
         dPad_LeftUp = controller.GetPressUp(dPad_Left);
-        dPad_LeftPressed = controller.GetPress(dPad_Left);
+        dPad_LeftPressed = controller.GetPress(dPad_Left);*/
 
         if (gripButtonDown)
         {
@@ -94,7 +94,7 @@ public class WandController : MonoBehaviour
             Debug.Log("trigger Button was released");
         }
 
-        if (dPad_UpDown || dPad_DownDown || dPad_RightDown || dPad_LeftDown)
+        /*if (dPad_UpDown || dPad_DownDown || dPad_RightDown || dPad_LeftDown)
         {
             Debug.Log("the dpad was pressed was pressed");
         }
@@ -102,6 +102,6 @@ public class WandController : MonoBehaviour
         if (dPad_UpUp || dPad_DownUp || dPad_RightUp || dPad_LeftUp)
         {
             Debug.Log("the dpad was released");
-        }
+        }*/
     }
 }
