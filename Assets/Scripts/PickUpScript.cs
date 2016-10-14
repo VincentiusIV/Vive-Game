e9bab3f85eb7a0816e718c7a775cc0ec27cb330a
Vincent Versnel <vincent@versnelhifi.nl>
2016-10-14 10:02:40 +0200

    Updated Pick Up script to no longer take a public variable as a reset position for collided object

--- Assets/Scripts/PickUpScript.cs
+++ Assets/Scripts/PickUpScript.cs
@@ -8,7 +8,8 @@ public class PickUpScript : MonoBehaviour {
     SteamVR_TrackedObject trackedObj;
     SteamVR_Controller.Device device;
 
-    public Transform sphere;
+    private GameObject colObj;
+    private Transform resPosColObj;
 
     // Use this for initialization
     void Awake ()
@@ -27,19 +28,27 @@ public class PickUpScript : MonoBehaviour {
 
         if (device.GetPressUp(SteamVR_Controller.ButtonMask.Grip))
         {
-            Debug.Log("You are holding touch on the touchpad");
-            sphere.transform.position = 
-            sphere.GetComponent<Rigidbody>().velocity = 
-            sphere.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            Debug.Log("You are holding touch on the grip button");
+            colObj.transform.position = 
+            colObj.GetComponent<Rigidbody>().velocity =
+            colObj.GetComponent<Rigidbody>().angularVelocity = resPosColObj.position;
+
+            resPosColObj = null;
         }
     }
 
     void OnTriggerStay(Collider col)
     {
+        if(resPosColObj == null && colObj == null)
+        {
+            resPosColObj = col.transform;
+            colObj = col.gameObject;
+        }
+
         Debug.Log("You have collided with " + col.name + " and activated OnTriggerStay");
         if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
         {
-            Debug.Log("You have collided with " + col.name + " while holding down Touch");
+            Debug.Log("You have collided with " + col.name + " while holding down the trigger");
             col.attachedRigidbody.isKinematic = true;
             col.gameObject.transform.SetParent(this.gameObject.transform);
         }
@@ -55,6 +64,7 @@ public class PickUpScript : MonoBehaviour {
                 tossObject(col.attachedRigidbody);
             }
             
+            //col.GetComponent<Rigidbody>()
         }
     }
 
