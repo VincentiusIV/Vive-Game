5ebfff9d355c3f933a0c1d627ab7cd957383f390
Solution6996 <jeroen.wilmink@hotmail.com>
2016-10-14 13:43:22 +0200

    Sceneloader added to controller (hopefully)

---
+++ Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ButtonHandler : MonoBehaviour {
+
+    public bool scene1;
+    public bool scene2;
+    public bool scene3;
+
+
+    void OnTriggerEnter(Collider other) {
+        if (other.tag == "VrController") {
+            if (scene1 == true) {
+                SceneManager.LoadScene("Game");
+            } else if (scene2 == true) {
+                SceneManager.LoadScene("Office Scene");
+            } else if (scene3 == true) {
+                SceneManager.LoadScene("Main Menu");
+            }
+        }
+
+    }
+
+
+}
