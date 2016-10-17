fa9bc5cd17d9bf56e46d41ff49dcee39ba725ba9
Solution6996 <jeroen.wilmink@hotmail.com>
2016-10-17 10:10:01 +0200

    Sceneloader update #2

--- Assets/Scripts/ButtonHandler.cs
+++ Assets/Scripts/ButtonHandler.cs
@@ -11,12 +11,19 @@ public class ButtonHandler : MonoBehaviour {
 
     void OnTriggerEnter(Collider other) {
         if (other.tag == "VrController") {
+            Debug.Log("Collider has VrController tag.");
             if (scene1 == true) {
+                Debug.Log("Scene1 == true");
                 SceneManager.LoadScene("Game");
+                Debug.Log("Game scene loaded");
             } else if (scene2 == true) {
+                Debug.Log("Scene 2 == true");
                 SceneManager.LoadScene("Office Scene");
+                Debug.Log("office scene loaded.");
             } else if (scene3 == true) {
+                Debug.Log("scene3 == true");
                 SceneManager.LoadScene("Main Menu");
+                Debug.Log("Main menu scene loaded.");
             }
         }
 
