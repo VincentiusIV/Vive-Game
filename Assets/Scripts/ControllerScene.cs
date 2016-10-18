d85b565744d434ccb3593788e07777cbb2421857
Vincent Versnel <vincent@versnelhifi.nl>
2016-10-18 13:54:07 +0200

    Added Laura's 3d body model to scenes

--- Assets/Scripts/ControllerScene.cs
+++ Assets/Scripts/ControllerScene.cs
@@ -7,26 +7,31 @@ public class ControllerScene : MonoBehaviour {
     SteamVR_TrackedObject obj;
 
     public GameObject ButtonHolder;
-
     public bool ButtonEnabled;
 
-
-	void Awake() {
+	void Awake()
+    {
         obj = GetComponent<SteamVR_TrackedObject>();
         ButtonHolder.SetActive(false);
         ButtonEnabled = false;
         Debug.Log("Awoken with ButtonEnabled set as:" + ButtonEnabled + " And the ButtonHolder set to: " + ButtonHolder.activeSelf);
-
 	}
 	
-	void Update () {
+	void Update ()
+    {
         var device = SteamVR_Controller.Input((int)obj.index);
-        if (device.GetPressDown(SteamVR_Controller.ButtonMask.ApplicationMenu)) {
-            if (ButtonEnabled == false) {
+
+        if (device.GetPressDown(SteamVR_Controller.ButtonMask.ApplicationMenu))
+        {
+            if (ButtonEnabled == false)
+            {
                 ButtonEnabled = true;
                 ButtonHolder.SetActive(true);
                 Debug.Log("ButtonEnabled and ButtonHolder active state set to TRUE");
-            } else if (ButtonEnabled == true) {
+
+            }
+            else if (ButtonEnabled == true)
+            {
                 ButtonEnabled = false;
                 ButtonHolder.SetActive(false);
                 Debug.Log("ButtonEnabled and ButtonHolder active state set to FALSE");
