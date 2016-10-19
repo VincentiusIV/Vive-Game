fe950f87e187a49d7c2503a272679f6fcbe00e6c
Vincent Versnel <vincent@versnelhifi.nl>
2016-10-19 13:48:18 +0200

    Object snapping final fix (IT WORKS ! YAY)

--- Assets/Scripts/PickUpScript.cs
+++ Assets/Scripts/PickUpScript.cs
@@ -26,19 +26,22 @@ public class PickUpScript : MonoBehaviour
         { return; } 
         else
         {
-            Debug.Log("You have collided with " + col.name + " and activated OnTriggerStay");
+            // runs when trigger is held down
             if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
             {
-                Debug.Log("You have collided with " + col.name + " while holding down the trigger");
                 col.attachedRigidbody.isKinematic = true;
                 col.gameObject.transform.SetParent(this.gameObject.transform);
 
+                /* This is for objects that have already been snapped into place by the player
+                 * canSnap is reset so the player can pick up the object that has been snapped before
+                 */
                 if (col.gameObject.GetComponent<CompareTags>().isColSnap == true)
                 {
                     col.gameObject.GetComponent<CompareTags>().canSnap = false;
                 }
             }
 
+            // runs when trigger is released
             if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
             {
                 Debug.Log("You have released Trigger while colliding with " + col.name);
@@ -49,6 +52,11 @@ public class PickUpScript : MonoBehaviour
                 {
                     tossObject(col.attachedRigidbody);
                 }
+
+                /* This basically checks if the object the controller is colliding with
+                 * is colliding with a SnapPosition.
+                 * This makes the object able to snap onto the SnapPosition when the trigger is released.
+                 */
                 if(col.gameObject.GetComponent<CompareTags>().isColSnap == true)
                 {
                     col.gameObject.GetComponent<CompareTags>().canSnap = true;
