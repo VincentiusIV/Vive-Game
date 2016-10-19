a2e0c15018c9f184d57df23fa50ea0404fa63018
Vincent Versnel <vincent@versnelhifi.nl>
2016-10-19 10:21:47 +0200

    Object snapping fix #1

--- Assets/Scripts/SnapScript.cs
+++ Assets/Scripts/SnapScript.cs
@@ -4,15 +4,22 @@ using System.Collections;
 public class SnapScript : MonoBehaviour
 {
     private Transform snapPosition;
+    public GameObject parent;
 
     void Start()
     {
         snapPosition = this.transform;
+
+        if(parent = null)
+        {
+            Debug.Log("No parent set for: " + this.gameObject.name);
+        }
     }
 
     public void SnapToPosition(GameObject _obj)
     {
         _obj.transform.position = snapPosition.position;
         _obj.transform.rotation = snapPosition.rotation;
+        _obj.transform.SetParent(parent.transform);
     }
 }
