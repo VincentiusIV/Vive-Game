1f52f9fa861de9434a79d7c542186351942be0a0
Vincent Versnel <vincent@versnelhifi.nl>
2016-11-06 16:17:18 +0100

    remaining fixes

--- Assets/Scripts/DecreasingProgressBar.cs
+++ Assets/Scripts/DecreasingProgressBar.cs
@@ -4,13 +4,9 @@ using System.Collections;
 public class DecreasingProgressBar : MonoBehaviour
 {
     public float progressValue;
-    public float decreasePerMs;
+    public float decreasePerSec;
 
-<<<<<<< HEAD
     private bool alive;
-=======
-    private bool runBar;
->>>>>>> 532ffb6ebecf7400c8602c7a1fb7a697bd594281
     private int effectiveness;
 
     private bool increase = true;
@@ -18,61 +14,40 @@ public class DecreasingProgressBar : MonoBehaviour
     // Use this for initialization
     void Start ()
     {
-<<<<<<< HEAD
         alive = true;
         effectiveness = 0;
-=======
-        runBar = true;
->>>>>>> 532ffb6ebecf7400c8602c7a1fb7a697bd594281
 
-        Debug.Log("runBar in Start() = " + runBar);
+	    if(progressValue == 0)
+        {
+            progressValue = 100;
+        }
         StartCoroutine(DecreasePerSecond());
         StartCoroutine(checkEffectiveness());
 	}
-
+	
 	IEnumerator DecreasePerSecond()
     {
-<<<<<<< HEAD
         while (progressValue != 0 && alive)
-=======
-        // runs continously while runBar == true
-        while (progressValue > 0 || progressValue < 100)
->>>>>>> 532ffb6ebecf7400c8602c7a1fb7a697bd594281
         {
-            progressValue -= decreasePerMs;
+            progressValue -= decreasePerSec;
+            yield return new WaitForSeconds((decreasePerSec * 2) / 10);
             Debug.Log("progress bar is at: " + progressValue + "%");
 
-            if (progressValue <= 0)
+            if(progressValue <= 0)
             {
                 Debug.Log("patient died! you lose");
-<<<<<<< HEAD
                 alive = false;
-=======
-                //runBar = false;
->>>>>>> 532ffb6ebecf7400c8602c7a1fb7a697bd594281
             }
-            if (progressValue >= 100)
-            {
-                Debug.Log("patient is alive! you win");
-                //runBar = false;
-            }
-            yield return new WaitForSeconds(1.0f);
         }
-        
     }
 
     public void increaseForPush()
     {
-<<<<<<< HEAD
         if(alive)
-=======
-        if (progressValue > 0 || progressValue < 100)
->>>>>>> 532ffb6ebecf7400c8602c7a1fb7a697bd594281
         {
             progressValue += effectiveness;
-            Debug.Log("You pushed the chest and improved health by: "+effectiveness);
+            Debug.Log("You pushed the chest");
 
-<<<<<<< HEAD
             if (progressValue > 100)
             {
                 Debug.Log("Patient is breathing again! you win");
@@ -83,22 +58,13 @@ public class DecreasingProgressBar : MonoBehaviour
         else
         {
             Debug.Log("Patient no longer needs pushing");
-=======
-            effectiveness = -5;
-            increase = true;
-            decrease = false;
->>>>>>> 532ffb6ebecf7400c8602c7a1fb7a697bd594281
         }
         
     }
 
     IEnumerator checkEffectiveness()
     {
-<<<<<<< HEAD
         while(alive)
-=======
-        while(progressValue > 0 || progressValue < 100)
->>>>>>> 532ffb6ebecf7400c8602c7a1fb7a697bd594281
         {
             if (increase)
             {
@@ -109,16 +75,19 @@ public class DecreasingProgressBar : MonoBehaviour
                     decrease = true;
                 }
             }
+
             if (decrease)
             {
                 effectiveness -= 1;
-                if (effectiveness <= -6)
+                if (effectiveness <= -4)
                 {
                     increase = true;
                     decrease = false;
                 }
             }
+
             yield return new WaitForSeconds(1/15);
+            Debug.Log("effectiveness = "+ effectiveness);
         } 
     }
 }
