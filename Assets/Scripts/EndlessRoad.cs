4980669e1fd1c906fa7bec298620a5819837cbea
Vincent Versnel <vincent@versnelhifi.nl>
2016-11-09 17:08:23 +0100

    Added ambulance main menu (features endless moving road), still needs the ambulance interior

---
+++ Assets/Scripts/EndlessRoad.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndlessRoad : MonoBehaviour
+{
+    /* A simple script that allows the road to be endless (taken from Vincent' racing game)
+     * 
+     * In the Update() function the z position is checked if it has surpassed
+     * 0 and/or destroyDistance. 
+     * 
+     * When a piece of road reaches 0, a new road is instantiated
+     * from the array of roads. 
+     * 
+     * When it surpasses destroyDistance, it is behind the player
+     * where it can no longer be seen and the gameobject is destroyed.
+     * */
+    public GameObject[] roads;
+    public bool repeatSpawning = false;
+    public float destroyDistance;
+
+    private bool spawnedNew = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (repeatSpawning)
+        {
+            if (transform.position.z < 0 && spawnedNew != true)
+            {
+                Instantiate(roads[Random.Range(0, roads.Length - 1)], new Vector3(0, 0, 59), Quaternion.identity);
+                spawnedNew = true;
+            }
+        }
+
+        if (transform.position.z < destroyDistance)
+        {
+            Destroy(this.gameObject);
+
+        }
+    }
+}
---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour
+{
+    // A simple script that moves an object along the z axis in a constant speed
+
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0, Time.deltaTime * -speed);
+    }
+}
