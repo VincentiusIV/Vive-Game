186527a9af663266da3765cd7497518ba1fee30a
Vincent Versnel <vincent@versnelhifi.nl>
2016-11-11 11:49:22 +0100

    Fixed hand movement

--- Assets/Scripts/InputScript.cs
+++ Assets/Scripts/InputScript.cs
@@ -46,20 +46,11 @@ public class InputScript : MonoBehaviour
     {
         if (col.tag == "VrController")
         { return; }
-        else if(col.CompareTag("InteractableArea"))
+        /*else if(col.CompareTag("InteractableArea"))
         {
             col.transform.position = new Vector3(col.transform.position.x, col.transform.position.y, this.transform.position.z);
             Debug.Log("collider tag is " + col.tag);
-            /*if (device.GetPressDown(SteamVR_Controller.ButtonMask.Grip))
-            {
-                Debug.Log("Grip is held down");
-                
-            }
-            if (device.GetPressUp(SteamVR_Controller.ButtonMask.Grip))
-            {
-                
-            }*/
-        }
+        }*/
         else if(col.tag != "InteractableArea")
         {
             // runs when trigger is held down
