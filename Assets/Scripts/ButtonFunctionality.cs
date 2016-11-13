8472ff63cf483c0d0d008a778aa2b523e13fc15e
Vincent Versnel <vincent@versnelhifi.nl>
2016-11-13 20:34:58 +0100

    Updated Road prefab with laura' houses, updates menu scene and script, merged MenuAnimator with ButtonFunctionality,

--- Assets/Scripts/ButtonFunctionality.cs
+++ Assets/Scripts/ButtonFunctionality.cs
@@ -4,9 +4,14 @@ using UnityEngine.SceneManagement;
 
 public class ButtonFunctionality : MonoBehaviour
 {
-    public bool SwitchScene;
+    public float upDistance;
+    public int animationSpeed;
+
+    public bool switchScene;
     public string sceneName;
     public bool exitGame;
+    public bool showSliders;
+
     /*
      * Method for button functionality such as Loading a new scene
      * runs when the button is colliding with the Vive Controller
@@ -15,7 +20,7 @@ public class ButtonFunctionality : MonoBehaviour
     {
         if (other.CompareTag("VrController"))
         {
-            if (SwitchScene)
+            if (switchScene)
             {
                 SceneManager.LoadScene(sceneName);
             }
@@ -23,6 +28,42 @@ public class ButtonFunctionality : MonoBehaviour
             {
                 Application.Quit();
             }
+            if(showSliders)
+            {
+                StartCoroutine(move(false));
+            }
+        }
+    }
+    void OnMouseEnter()
+    {
+        Debug.Log("mouse is over");
+        if (showSliders)
+        {
+            if(transform.parent.transform.position.y >= upDistance)
+            {
+                StartCoroutine(move(true));
+            }
+            else if(transform.parent.transform.position.y <= 0)
+            {
+                StartCoroutine(move(false));
+            }
+            
+        }
+    }
+
+    IEnumerator move(bool down)
+    {
+        for (int i = 0; i < animationSpeed; i++)
+        {
+            float yValue = (upDistance / animationSpeed);
+
+            if (down)
+            {
+                yValue *= -1;
+            }
+
+            transform.parent.transform.Translate(new Vector3(0.0f, yValue, 0.0f));
+            yield return new WaitForSeconds(0.0f);
         }
     }
 }
