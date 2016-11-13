ec51f735cfa1d7684871f7cf8500c86772b158f6
Vincent Versnel <vincent@versnelhifi.nl>
2016-11-13 15:33:26 +0100

    refactored sceneloader script

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -4,35 +4,19 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-
-    public bool GameScene;
-    public bool OfficeScene;
-    public bool MenuScene;
-    public bool AnimationTest;
-    public bool Field;
-    
+    public bool SwitchScene;
+    public string sceneName;
     /*
      * Method used for the switching of scenes. OnTriggerEnter is called whenever collision happens
      * A check is the performed to see if the colliding object is the controller, if true it checks which scene is being tapped and then it's loaded.     * 
      */
-    void OnTriggerEnter(Collider other) {
-        if (other.tag == "VrController") {
-            if (GameScene) {
-                SceneManager.LoadScene("Game Test Scene");
-            }
-            else if (OfficeScene) {
-                SceneManager.LoadScene("Office Scene");
-            }
-            else if (MenuScene) {
-                SceneManager.LoadScene("Main Menu");
-            }
-            else if (AnimationTest)
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("VrController"))
+        {
+            if (SwitchScene)
             {
-                SceneManager.LoadScene("AnimationTest");
-            }
-            else if (Field)
-            {
-                SceneManager.LoadScene("Field");
+                SceneManager.LoadScene(sceneName);
             }
         }
     }
