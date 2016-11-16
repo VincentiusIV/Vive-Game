90bb8d044b593d9ec5c0250f0175acc65b5727af
Vincent Versnel <vincent@versnelhifi.nl>
2016-11-16 12:09:26 +0100

    added easter egg (cake is a lie)

---
+++ Assets/Scripts/EasterEgg.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EasterEgg : MonoBehaviour
+{
+    private AudioSource sound;
+	// Use this for initialization
+	void Awake ()
+    {
+        sound = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter()
+    {
+        sound.Play();
+    }
+}
